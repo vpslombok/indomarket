62e54b329a738131ed54d64e7398f99eb0f227e7
vpslombok <vpslombokntb@gmail.com>
2024-01-03 11:25:52 +0800

    memperbaiki erorr

--- application/controllers/User.php
+++ application/controllers/User.php
@@ -3,8 +3,11 @@ defined('BASEPATH') or exit('No direct script access allowed');
 
 class User extends CI_Controller
 {
+    public function __construct() {
+        parent::__construct();
+        $this->load->model('Model_barang');
+    }
     
-
     public function index()
     {
         $data['user'] = $this->db->get_where('user', ['email' => $this->session->userdata('email')])->row_array();
